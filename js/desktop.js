67e3260bfdbd187730200b8fe096d9d25df6979a
yangwen <1052045476@qq.com>
2018-01-09 18:12:11 +0800

    init

---
+++ js/desktop.js
@@ -0,0 +1,36 @@
+var shortcuts = [
+{	
+	mode: 'inside',
+	name: 'Asgc Blog',
+	icon: './img/icon/asgc_blog.ico',
+	url: 'http://blog.aoshiguchen.com',
+	title: '傲世孤尘的博客'
+},
+{	
+	mode: 'inside',
+	name: 'How2u Blog',
+	icon: './img/icon/how2u_blog.ico',
+	url: 'http://blog.ee2j.com/',
+	title: '视佬的博客'
+},
+{	
+	mode: 'inside',
+	name: 'Battcn Blog',
+	icon: './img/icon/battcn_blog.png',
+	url: 'http://blog.battcn.com/',
+	title: '唐亚峰 的博客'
+},
+{	
+	mode: 'outside',
+	name: 'Asgc Github',
+	icon: './img/icon/github.png',
+	url: 'https://github.com/aoshiguchen',
+},
+{	
+	mode: 'inside',
+	name: '开源中国',
+	icon: './img/icon/kyzg.png',
+	url: 'https://www.oschina.net',
+	title: '开源中国'
+},
+];
